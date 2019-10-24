fa275475075e0bd5f452b2cdab4d695f2e012588
matthewl-sudo <mlarose4892@gmail.com>
2019-10-24 17:11:05 -0600

    Change bs-index.html to index.php

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style2.css">
+    <script src="https://kit.fontawesome.com/f5a250744a.js"></script>
+</head>
+<body>
+    <header class="bgimg">
+        <div class="jumbotron">
+            <a href="index.html"><img id="logo" src="images/okotoks-pizza-logo.png" title="Home" alt="logo";></a>
+             <h1 class="container">Simply Delicious</h1>
+            <p>...</p>
+            <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
+        </div>
+    </header>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+        <a class="navbar-brand" href="bs-menu.html">Okotoks Pizza</a>
+        <button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav justify-content-between">
+                <li class="nav-item active">
+                    <a class="nav-link" href="bs-index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="bs-menu.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Menu
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Pizzas</a>
+                        <a class="dropdown-item" href="#">Calzones</a>
+                        <a class="dropdown-item" href="#">Pastas</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="bs-about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="bs-contact.html">Contact</a>
+                </li>
+                <li class="nav-item" >
+                    <a class="contact nav-link" href="tel:(403) 555-5555">Call Us Now (403)555-5555!</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <section id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="images/pizzacrust.jpg" alt="First slide">
+                <div class="carousel-caption">
+                    <h2>You asked, we listened.</br>Now introducing a gluten-free pizza crust option</h2>
+                    <p>(Starting the 1st of July)</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100 center" src="images/pizzadelivery.jpg" alt="Second slide">
+                <div class="carousel-caption center">
+                    <h2>Free Delivery on Weekends on Orders over 50$</h2>
+                    <p>(Excludes weekdays)</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/pizzaslider.jpg" alt="Third slide">
+                <div class="carousel-caption">
+                    <h2></h2>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </section>
+
+    <aside id="icon">
+        <a href="tel:(403)555-5555"><img src="images/phone-icon1.png" alt="phone-icon">Call us</a>
+
+    </aside>
+    <footer id="footer" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3 container">
+                <p>Copyright (c) 2018 Copyright Holder All Rights Reserved.</p>
+            </div>
+            <div class="col-sm-3">
+                <ul class="list-unlisted">
+                    <li><a href="#"><i class="fab fa-facebook-square"></i> Facebook</a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <ul class="list-group">
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Catering</a></li>
+                    <li><a href="#">Nutritional Information</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
